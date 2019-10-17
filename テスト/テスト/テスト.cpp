210fa809e5d9c3709e23b14721dfc306fcdba80b
yuuki_L2 <yuukisnow85pay@gmail.com>
2019-10-17 21:49:47 +0900

    自宅テスト

--- テスト/テスト/テスト.cpp
+++ テスト/テスト/テスト.cpp
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <conio.h>
+#include<iostream>
 
 int main(void)
 {
 	printf("テステス、草に草をはやすな。\n");
 
-
+	int numA;
 	//kimuratuiki
 
 	//lkjoijlkjoij;
 
 	printf("時雨はかわいい！！！！！！。\n");
 	printf("ゲッダン☆\n");
-	
+	cin >> numA;
+	cout << numA<<"番号\n";
 	
 	_getch();
 	return 0;
